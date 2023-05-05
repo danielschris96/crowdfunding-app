12a20f8b46d093bb810cd62d06349a7379e3efac
Daniel Lam <Dann.lam@gmail.com>
2023-05-05 14:15:34 -0700

    Strted handle bars

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+// const exphdb = require('./express-handlebars');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -14,8 +15,8 @@ const sess = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
